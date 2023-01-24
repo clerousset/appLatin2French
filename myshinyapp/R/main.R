b4afd900025df9229daa3e86cb1edf09c5f8cb13
clerousset <clerousset@protonmail.com>
2023-01-24 19:00:52 +0100

    Update main.R

--- myshinyapp/R/main.R
+++ myshinyapp/R/main.R
@@ -1,6 +1,6 @@
 
 runApp <- function(){
   #myshinyapp::populate_table(bucket = "avouacr", object = "diffusion/shiny-template/quakes.csv")
-  appDir <- system.file("app", package = "myshinyapp")
+  appDir <- system.file("app", package = "latin2french")
   shiny::runApp(appDir, display.mode = "normal")
 }
