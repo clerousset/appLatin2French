26869e9bd869fb144c2f7dffe6f455aeda59ee79
clerousset <clerousset@protonmail.com>
2023-01-27 18:30:16 +0100

    Add files via upload

---
+++ applatin2french/inst/app/test_affiche.R
@@ -0,0 +1,49 @@
+library("data.table")
+install.packages("kableExtra")
+library("kableExtra")
+dt <- latin_to_french2("c\u0101\u0301m\u0115ra", rules=rules)
+dt[, century:=fcase(
+  is.infinite(date), "Preliminaries",
+  date < 100, "1st century AD",
+  date < 200, "2nd century AD",
+  date < 300, "3rd century AD",
+  date>=300, paste0(floor(date/100)+1,"th century AD")
+)]
+
+dt <- dt[data.table(century = c("Preliminaries","1st century AD",
+                       "2nd century AD","3rd century AD",
+           paste0(4:18, "th century AD"))),on =.(century)]
+
+dt[,period:=fcase(
+  is.infinite(date), "Preliminaries",
+  century %chin% c("1st century AD","2nd century AD","3rd century AD"),
+  "Common Romance Transformation",
+  default = "French transformations")]
+
+dt[,orthograph_fixation:=
+  century %chin% c("9th century AD","10th century AD")]
+
+kable(dt[,.(century, explanation, word, rule_id)], format.args=list(na.encode=TRUE)) %>%
+  pack_rows(
+    group_label = "Preliminaries",
+    start_row = min(which(dt$period == "Preliminaries")),
+    end_row = max(which(dt$period == "Preliminaries")),
+    background = "violet") %>%
+  row_spec(row = which(dt$period == "Preliminaries"), background = "violet") %>%
+  pack_rows(
+    group_label = "Common Romance Transformation",
+    start_row = min(which(dt$period == "Common Romance Transformation")),
+    end_row = max(which(dt$period == "Common Romance Transformation")),
+    background = "orange") %>%
+  row_spec(row = which(dt$period == "Common Romance Transformation"), background = "orange") %>%
+  pack_rows(
+    group_label = "French transformations",
+    start_row = min(which(dt$period == "French transformations")),
+    end_row = max(which(dt$period == "French transformations")),
+    background = "skyblue") %>%
+  row_spec(row = which(dt$period == "French transformations"), background = "skyblue") %>%
+  
+  kableExtra::kable_styling()
+________________________________________
+Nos ministères agissent pour un développement durable.
+Préservons l'environnement : n'imprimons que si nécessaire !
