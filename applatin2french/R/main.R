1986cf1ec237fbb6ffab4adb467cb3debc98e686
clerousset <clerousset@gmail.com>
2023-02-15 11:10:09 +0000

    runApp in package

--- applatin2french/R/main.R
+++ applatin2french/R/main.R
@@ -1,4 +1,4 @@
-
+#' @export
 runApp <- function(){
   #myshinyapp::populate_table(bucket = "avouacr", object = "diffusion/shiny-template/quakes.csv")
   appDir <- system.file("app", package = "applatin2french")
